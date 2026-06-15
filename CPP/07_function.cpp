698b258ecdac9f1e6bbb60271e194c81db595593
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-06-16 00:09:09 +0530

    Add files via upload

---
+++ CPP/07_function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float();
+    return 0;
+}
+float calc(int a,int b){
+    a = 10;
+    b = 3;
+    float c = a/b;
+    return c;
+}
