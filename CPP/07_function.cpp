c29a9e5dfea636ff04588ecdd9ed30bdf58a247d
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-06-16 18:44:25 +0530

    Pass by value and pass by reference

--- CPP/07_function.cpp
+++ CPP/07_function.cpp
@@ -1,13 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    float();
-    return 0;
+void printName(){
+    cout << "Function to calc the >>>>>>>>.";
 }
-float calc(int a,int b){
-    a = 10;
-    b = 3;
-    float c = a/b;
-    return c;
+int calc(int a, int b){
+    return a+b;
+}
+int min_max(int num1, int num2){
+    if(num1 >= num2) return num1;
+    else return num2;
+
+}
+int main(){
+    printName();
+    int result = calc(5,5);
+    cout << result;
+    int num1,num2;
+    cin >> num1 >> num2;
+    int mini = min_max(num1, num2);
+    cout << mini;
+    return 0;
 }
